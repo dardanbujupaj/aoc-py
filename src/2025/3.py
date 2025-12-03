1b9e88f8b6b6e51c2b6338f9e042c5f27c459727
Dardan Bujupaj <dardan@ax.tech>
2025-12-03 10:16:41 +0100

    part 2

--- src/2025/3.py
+++ src/2025/3.py
@@ -2,27 +2,36 @@ from api import get_input
 
 input = get_input(2025, 3)
 
-# input = """987654321111111
-# 811111111111119
-# 234234234234278
-# 818181911112111"""
-
 parsed = [[int(digit) for digit in list(line)] for line in input.splitlines()]
+
+
+def max_joltage(bank: list[int], digits=2) -> int:
+    for i in range(9, 0, -1):
+        candidates = bank[: len(bank) - (digits - 1)]
+
+        for index, d in enumerate(candidates):
+            if d == i:
+                sub = max_joltage(bank[index + 1 :], digits - 1) if digits > 1 else 0
+                return d * pow(10, digits - 1) + sub
+
+    return 0
 
 
 def part1() -> None:
     print("part1")
-    total = 0
-    for bank in parsed:
-        first = max(bank[:-1])
-        index = bank.index(first)
-        second = max(bank[index+1:])
-        print(f"first: {first}, index: {index}, second: {second}")
-        total += first * 10 + second
+
+    total = sum([max_joltage(bank) for bank in parsed])
 
     print(f"total: {total}")
 
 
-
 def part2() -> None:
     print("part2")
+
+    total = sum([max_joltage(bank, 12) for bank in parsed])
+
+    print(f"total: {total}")
+
+
+if __name__ == "__main__":
+    part1()
